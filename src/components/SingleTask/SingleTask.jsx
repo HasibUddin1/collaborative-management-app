d301ba53b661292d2f80871f7a15c5b2539baa41
IconicLuck <uhasib959@gmail.com>
2023-09-09 12:32:53 +0600

    implemented task filtering

--- src/components/SingleTask/SingleTask.jsx
+++ src/components/SingleTask/SingleTask.jsx
@@ -1,8 +1,23 @@
 
 
-const SingleTask = ({ task }) => {
+const SingleTask = ({ task, setTasks }) => {
 
-    const { taskTitle, taskStatus, taskPriority, taskDescription, dueDate, assign } = task
+    const { id, taskTitle, taskStatus, taskPriority, taskDescription, dueDate, assign } = task
+
+    const handleCompleted = id => {
+        const tasks = JSON.parse(localStorage.getItem("tasks")) || []
+
+        const taskToUpdate = tasks.find(task => task.id === id)
+
+        if (taskToUpdate) {
+            taskToUpdate.taskStatus = 'Completed'
+            localStorage.setItem("tasks", JSON.stringify(tasks))
+            setTasks(tasks)
+        }
+        else {
+            console.log("Task not found")
+        }
+    }
 
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
@@ -13,7 +28,11 @@ const SingleTask = ({ task }) => {
                 <p><span className="font-semibold">Task Priority:</span> {taskPriority}</p>
                 <p><span className="font-semibold">Due Date:</span> {dueDate}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-warning">Mark as Completed</button>
+                    {
+                        taskStatus === 'Completed' ?
+                            <button className="btn btn-neutral w-fit">Completed</button> :
+                            <button onClick={() => handleCompleted(id)} className="btn btn-warning">Mark as Completed</button>
+                    }
                 </div>
                 <p className="text-end flex flex-col"><span className="font-semibold">Assigned To:</span> {assign}</p>
             </div>
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -8,26 +8,62 @@ const Home = () => {
 
     const [tasks, setTasks] = useState([])
 
+    const [filter, setFilter] = useState('all')
+
     useEffect(() => {
         const storedTasks = JSON.parse(localStorage.getItem("tasks")) || []
         setTasks(storedTasks)
     }, [])
 
+    const filteredTasks = tasks.filter(task => {
+
+        console.log(filter)
+
+        if (filter === 'all') {
+            return true
+        }
+
+        if (filter === 'Completed') {
+            return task.taskStatus === 'Completed'
+        }
+
+        if (filter === 'In Progress') {
+            return task.taskStatus === 'In Progress'
+        }
+
+        if (filter === 'Pending') {
+            return task.taskStatus === 'Pending'
+        }
+
+        return true
+    })
+
     return (
-        <div>
+        <div className="p-5">
+            <div>
+                <h2 className="text-xl font-semibold">Task List</h2>
+                <select className="border-2 border-black rounded" onChange={(e) => setFilter(e.target.value)}>
+                    <option value="all">All</option>
+                    <option value="Completed">Completed</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Pending">Pending</option>
+                </select>
+            </div>
             {
-                tasks.length === 0 ? 
-                <h1 className="text-center text-4xl font-semibold">You do not have any tasks now. Create your task by going to <Link className="text-blue-500 hover:underline" to='/addATask'>Add A Task</Link></h1> :
-                <>
-                    <div className="grid xl:grid-cols-3 gap-5">
-                        {
-                            tasks.map(task => <SingleTask
-                                key={task.id}
-                                task={task}
-                            ></SingleTask>)
-                        }
-                    </div>
-                </>
+                tasks.length === 0 ?
+                    <h1 className="text-center text-4xl font-semibold">You do not have any tasks now. Create your task by going to <Link className="text-blue-500 hover:underline" to='/addATask'>Add A Task</Link></h1> :
+                    <>
+                        <div className="grid xl:grid-cols-3 gap-5">
+                            {
+                                filteredTasks.map(task => <SingleTask
+                                    key={task.id}
+                                    task={task}
+                                    tasks={tasks}
+                                    setTasks={setTasks}
+                                ></SingleTask>)
+                            }
+                        </div>
+                    </>
             }
         </div>
     );
